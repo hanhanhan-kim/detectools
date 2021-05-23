711e98abc096a35c52a909a930e6fab444bf8e9b
Han Kim <hankim@caltech.edu>
2021-05-23 10:24:28 -0700

    save test predictions to json

--- detectools/make_predictions.py
+++ detectools/make_predictions.py
@@ -1,6 +1,8 @@
 import random
 from os.path import expanduser, join
+import json
 
+import numpy as np
 import cv2
 from detectron2.engine import DefaultPredictor
 from detectron2.config import CfgNode
@@ -19,6 +21,7 @@ def main(config):
     testing_thresh = float(config["make_predictions"]["testing_thresh"])
     scale = float(config["make_predictions"]["scale"])
     number_of_imgs = int(config["make_predictions"]["number_of_imgs"])
+    do_show = config["make_predictions"]["do_show"]
 
     if not 0 < testing_thresh < 1:
         raise ValueError(f"The testing threshold, {testing_thresh}, must be between 0 and 1.")
@@ -40,6 +43,7 @@ def main(config):
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = testing_thresh   
     # cfg.DATASETS.TEST = ("val_data", ) # should already be saved from train_model.py
 
+    print("Generating predictor ...")
     predictor = DefaultPredictor(cfg)
 
     # Select random images to visualize the prediction results:
@@ -47,19 +51,48 @@ def main(config):
 
         id = d["image_id"]
         img = cv2.imread(d["file_name"])
-        out = predictor(img)
-        visualizer = Visualizer(img[:, :, ::-1], 
-                                metadata=metadata, 
-                                scale=scale, 
-                                instance_mode=ColorMode)
-        visualizer = visualizer.draw_instance_predictions(out["instances"].to("cpu"))        
+        outputs = predictor(img)
 
-        cv2.imshow(f"prediction on image {id}", visualizer.get_image()[:, :, ::-1])
-        print(f"Press any key to go to the next image ({i+1}/{number_of_imgs}) ...")
+        if do_show:
+            
+            # Visualize:
+            visualizer = Visualizer(img[:, :, ::-1], 
+                                    metadata=metadata, 
+                                    scale=scale, 
+                                    instance_mode=ColorMode)
+            visualizer = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))        
 
-        key = cv2.waitKey(0) & 0xFF
-        if key == ord("q"):
-            print("Quitting ...")
-            break
+            cv2.imshow(f"prediction on image {id}", visualizer.get_image()[:, :, ::-1])
+            print(f"Press any key to go to the next image ({i+1}/{number_of_imgs}) ...")
 
-        cv2.destroyAllWindows()
+            key = cv2.waitKey(0) & 0xFF
+            if key == ord("q"):
+                print("Quitting ...")
+                break
+
+            cv2.destroyAllWindows()
+
+        # Save the predicted box coords and scores to a dictionary:
+        test_preds = {}
+        preds = outputs['instances'].to('cpu')
+        boxes = preds.pred_boxes
+        scores = preds.scores
+        num_boxes = np.array(scores.size())[0]
+        all_boxes = []
+
+        for i in range(0, num_boxes):
+            coords = boxes[i].tensor.numpy()    	
+            score = float(scores[i].numpy())
+            all_boxes.append([int(coords[0][0]), 
+                              int(coords[0][1]), 
+                              int(coords[0][2]), 
+                              int(coords[0][3]), 
+                              score])
+
+        test_preds[d["file_name"]] = all_boxes
+
+    # Write the dictionary to a json:
+    output_json = join(model_root, "test_preds.json")
+    with open(output_json, "w") as f:
+        json.dump(test_preds, f)
+    print(f"Saved predictions to {output_json}")
